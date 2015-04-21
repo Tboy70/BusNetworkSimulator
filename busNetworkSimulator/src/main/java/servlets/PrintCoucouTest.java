40dfd63986746a111891ee2607cfc790cb2118d1
Petrol- <petrol@petrol-laptop>
2015-04-21 02:31:25 +0200

    Added a logger : logback To use it, import : -org.slf4j.Logger; -org.slf4j.LoggerFactory;
    Add an attribute in each class you will log things :
    private final static Logger log =
    LoggerFactory.getLogger("NameOfTheLoggerWhichCanBeAnyThingYouWant");

--- busNetworkSimulator/src/main/java/servlets/PrintCoucouTest.java
+++ busNetworkSimulator/src/main/java/servlets/PrintCoucouTest.java
@@ -1,39 +1,49 @@
 package servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Servlet implementation class PrintCoucouTest
  */
 public class PrintCoucouTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PrintCoucouTest() {
-        super();
-        System.out.println("Coucou Constructor");
-        // TODO Auto-generated constructor stub
-    }
+	private final static Logger log = LoggerFactory
+			.getLogger("fr.utbm.info.gl52.busnetworksimulator.servlet");
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public PrintCoucouTest() {
+		super();
+		System.out.println("Coucou Constructor");
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Coucou DoGet");
+		log.debug("Coucou");
 		response.getWriter().println("Coucou");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
