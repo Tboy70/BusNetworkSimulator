6e3553613418f797f39995563d332029a1a2fe26
Alexandre Guyon <alex.guyon78@gmail.com>
2015-05-27 14:21:46 +0200

    - Created parser interface

--- src/fr/utbm/info/gl52/Parser/IParser.java
+++ src/fr/utbm/info/gl52/Parser/IParser.java
@@ -1,9 +1,19 @@
 package fr.utbm.info.gl52.Parser;
 
+import fr.utbm.info.gl52.Collection.graph.IEdge;
+import fr.utbm.info.gl52.Collection.graph.IGraph;
+import fr.utbm.info.gl52.Collection.graph.INode;
 
 /**
  * 
  */
-public interface IParser {
-
+public interface IParser<N extends INode<?,N>,E extends IEdge<?,E>> extends Runnable {
+	
+	public void setFile(String file);
+	
+	public void run();
+	
+	public boolean hasFinished();
+	
+	public IGraph<N,E> getData();
 }
