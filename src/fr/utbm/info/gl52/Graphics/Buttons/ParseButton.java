97763c8083b5fa8b9bac961034476fcea85937ed
Alexandre Guyon <alex.guyon78@gmail.com>
2015-05-27 15:56:33 +0200

    - Parsing display on button ok

--- src/fr/utbm/info/gl52/Graphics/Buttons/ParseButton.java
+++ src/fr/utbm/info/gl52/Graphics/Buttons/ParseButton.java
@@ -2,8 +2,13 @@ package fr.utbm.info.gl52.Graphics.Buttons;
 
 import java.awt.event.ActionEvent;
 
-public class ParseButton extends ButtonComponent {
+import fr.utbm.info.gl52.Collection.graph.IEdge;
+import fr.utbm.info.gl52.Collection.graph.INode;
+import fr.utbm.info.gl52.Parser.IParser;
+import fr.utbm.info.gl52.Parser.ParserShapeFile;
 
+public class ParseButton extends ButtonComponent {
+	
 	public ParseButton(String text, int x, int y, int h, int w) {
 		super(text, x, y, h, w);
 		// TODO Auto-generated constructor stub
@@ -12,10 +17,12 @@ public class ParseButton extends ButtonComponent {
 	@Override
 	public void action(ActionEvent evt) {
 		this.parseDefaultFile();
+		// Load
+		// 
 	}
 	
 	private void parseDefaultFile(){
-		// TODO
+		IParser<INode<Float>, IEdge<Integer>> parser = new ParserShapeFile<Float, Integer>("resources/test.shp", "resources/test.shx");
 	}
 
 }
--- src/fr/utbm/info/gl52/Parser/ParserShapeFile.java
+++ src/fr/utbm/info/gl52/Parser/ParserShapeFile.java
@@ -1,19 +1,132 @@
 package fr.utbm.info.gl52.Parser;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+import fr.utbm.info.gl52.TestShapeFileParser.DisplayShapeFileFactory;
 import fr.utbm.info.gl52.Collection.graph.IEdge;
 import fr.utbm.info.gl52.Collection.graph.INode;
+import fr.utbm.set.attr.AttributeProvider;
+import fr.utbm.set.attr.AttributeValue;
+import fr.utbm.set.attr.HeapAttributeProvider;
+import fr.utbm.set.io.shape.AbstractElementFactory;
+import fr.utbm.set.io.shape.ESRIPoint;
+import fr.utbm.set.io.shape.ShapeFileIndexReader;
+import fr.utbm.set.io.shape.ShapeFileReader;
+import fr.utbm.set.io.shape.ShapeMultiPatchType;
 
 /**
  * 
  */
 public class ParserShapeFile<Dn,De> extends AbstractParser<INode<Dn>,IEdge<De>> {
-
-	public ParserShapeFile(String file) {
-		super(file);
+	
+	private URL shpResource;
+	private URL shxResource;
+	
+	public ParserShapeFile(String shp, String shx) {
+		super(shp);
+		try {
+			this.shpResource = new URL("file:///C:/Users/Alexandre/Desktop/gitEclipse/BusNetworkSimulator/"+ shp);
+			this.shxResource = new URL("file:///C:/Users/Alexandre/Desktop/gitEclipse/BusNetworkSimulator/"+ shx);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void run(){
-		// TODO Implement run
+		try {
+			ShapeFileIndexReader shxReader = new ShapeFileIndexReader(this.shxResource);
+			ShapeFileReader<Dn> reader = new ShapeFileReader<Dn>(this.shpResource, null, shxReader, new DisplayShapeFileFactory());
+			
+			Iterator<Dn> i = reader.iterator(); 
+			while(i.hasNext()) // Read
+				i.next();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	public class DisplayShapeFileFactory extends AbstractElementFactory<Dn> {
+
+		public DisplayShapeFileFactory() {
+			//
+		}
+
+		public AttributeProvider createAttributeProvider(int elementIndex) {
+			System.out.println("createAttributeProvider : "+ elementIndex);
+			return new HeapAttributeProvider();
+		}
+		
+		public Dn createPolyline(AttributeProvider provider, int shapeIndex, int[] parts, ESRIPoint[] points, boolean hasZ) {
+			System.out.print("createPolyline : " + provider.toString() + ", shapeIndex : " + shapeIndex + ", parts : [");
+			for(int i = 0 ; i < parts.length ; ++i)
+				System.out.print(parts[i]+", ");
+			System.out.print("], points : [");
+			for(int i = 0 ; i < points.length ; ++i)
+				System.out.print(points[i]+", ");
+			System.out.println("], hasZ : "+ hasZ);
+			return null;
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public Dn createMultiPoint(AttributeProvider provider, int shapeIndex, ESRIPoint[] points, boolean hasZ) {
+			System.out.print("createMultiPoint : " + provider.toString() + ", shapeIndex : " + shapeIndex + ", points : [");
+			for(int i = 0 ; i < points.length ; ++i)
+				System.out.print(points[i]+", ");
+			System.out.println("], hasZ : "+ hasZ);
+			return null;
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public Dn createPoint(AttributeProvider provider, int shape_index, ESRIPoint point) {
+			System.out.print("createPoint : " + provider.toString() + ", shapeIndex : " + shape_index + ", point : "+ point);
+			return null;
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public Dn createMultiPatch(AttributeProvider provider, int shapeIndex, int[] parts, ShapeMultiPatchType[] partTypes, ESRIPoint[] points) {
+			System.out.print("createMultiPatch : " + provider.toString() + ", shapeIndex : " + shapeIndex + ", parts : [");
+			for(int i = 0 ; i < parts.length ; ++i)
+				System.out.print(parts[i]+", ");
+			System.out.print("], points : [");
+			for(int i = 0 ; i < points.length ; ++i)
+				System.out.print(points[i]+", ");
+			System.out.print("], partType : [");
+			for(int i = 0 ; i < partTypes.length ; ++i)
+				System.out.print(partTypes[i]+", ");
+			System.out.println("], hasZ");
+			return null;
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public void putAttributeIn(Dn element, String attributeName, AttributeValue value) {
+			System.out.println("putAttributeIn : "+ element + ", attributename :"+attributeName + ", AttributeValue : " + value);
+		}
+
+		@Override
+		public Dn createPolygon(AttributeProvider provider, int shapeIndex, int[] parts, ESRIPoint[] points, boolean hasZ) {
+			System.out.print("Create Polygon : " + provider.toString() + ", shapeIndex : " + shapeIndex + ", parts : [");
+			for(int i = 0 ; i < parts.length ; ++i)
+				System.out.print(parts[i]+", ");
+			System.out.print("], points : [");
+			for(int i = 0 ; i < points.length ; ++i)
+				System.out.print(points[i]+", ");
+			System.out.println("], hasZ : "+ hasZ);
+			return null;
+		}
+	}
+
 
 }
