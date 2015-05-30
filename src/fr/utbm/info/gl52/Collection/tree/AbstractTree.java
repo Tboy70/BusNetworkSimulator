578f1121e7ceb6b31506ae5244ff77e3cb91159c
Alexandre Guyon <alex.guyon78@gmail.com>
2015-05-30 15:43:01 +0200

    Tree has no children whereas treenode has.

--- src/fr/utbm/info/gl52/Collection/tree/AbstractTree.java
+++ src/fr/utbm/info/gl52/Collection/tree/AbstractTree.java
@@ -3,7 +3,7 @@ package fr.utbm.info.gl52.Collection.tree;
 public abstract class AbstractTree<D, N extends AbstractTreeNode<D,N> > implements ITree<D, N> {
 
 	protected N root;
-	protected N[] children;
+	
 	public AbstractTree(N root)
 	{
 		this.root = root;
