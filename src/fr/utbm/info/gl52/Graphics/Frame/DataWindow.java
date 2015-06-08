5c28fcad97ae4abf21e510c7bf87689ce97d29fb
Alexandre Guyon <alex.guyon78@gmail.com>
2015-06-08 22:44:11 +0200

    - added mouselistener

--- src/fr/utbm/info/gl52/Graphics/Frame/DataWindow.java
+++ src/fr/utbm/info/gl52/Graphics/Frame/DataWindow.java
@@ -1,6 +1,8 @@
 package fr.utbm.info.gl52.Graphics.Frame;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -16,9 +18,23 @@ public class DataWindow extends AbstractFrame {
 	
 	private JTableBusLineModel busLine;
 	
+	private MouseAdapter mouse = new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e){
+			int row = table.rowAtPoint(e.getPoint());
+			int col = table.columnAtPoint(e.getPoint());
+			if(e.getClickCount() >= 2){ // handle double click
+				// Go to itineraire
+			}else{ // Single click
+				// TODO display traffic
+			}
+		}
+	};
+	
 	public DataWindow(String title, int h, int w) {
 		super(title, h, w);
 		
+		// New model
 		this.busLine = new JTableBusLineModel(); 
 		
 		// Create new table
@@ -27,7 +43,9 @@ public class DataWindow extends AbstractFrame {
 		// Add it to layout
 		getContentPane().add(new JScrollPane(this.table), BorderLayout.CENTER);
 		
-		// Define the minimum size
+		this.table.addMouseListener(mouse);
+		
+		// Define the minimum size of the window
 		this.pack();
 	}
 
